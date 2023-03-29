fe33b07f257a02c21c434517534f9dfc3466737c
franciscobmacedo <franciscovcbm@gmail.com>
2023-03-29 11:27:43 +0100

    setup main patch app structure
    base class for the App, View, Controller, Page and some Components

---
+++ python/patch/app/__init__.py
@@ -0,0 +1,3 @@
+from .app import Patch, Page
+from .components import Button, Chart, Component, Paragraph, TextInput
+from .view import View
---
+++ python/patch/app/page.py
@@ -0,0 +1,13 @@
+from typing import Union, Mapping, Iterable, Callable
+from .components import Component
+from .view import View
+
+
+class Page:
+    def __init__(
+        self, name: str, controller: Mapping, components: Iterable[Component]
+    ) -> None:
+        self.name = name
+        self.controller = controller  # TODO - extend controller parsing to accept mapping and str: Union[Controller, Mapping, str]
+        self.components = components  # TODO - extend components parsing to also accept a callable: Union[Iterable[Component], Callable]
+        self.view: View = View(name=name, components=components)
